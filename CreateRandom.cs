6474ce65503c6fe47ae015b6fca5a93286dac8a2
Michcic1 <62814182+Michcic1@users.noreply.github.com>
2020-03-29 01:20:59 +0100

    Add files via upload
    Random Generation Concept

---
+++ CreateRandom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRandom : MonoBehaviour
+{
+    public List<GameObject> LiGoSpawn = new List<GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject goToSpawn = LiGoSpawn[Random.Range(0, LiGoSpawn.Count)];
+        Instantiate(goToSpawn, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
